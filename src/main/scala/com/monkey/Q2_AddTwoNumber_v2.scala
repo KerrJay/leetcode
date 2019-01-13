14a5012bd405a68c28baa0dfce495d94a7c7bc37
MonkeyHypers <haojie.hu@hypers.com>
2019-01-13 23:06:09 +0800

    Add code annotation

--- src/main/scala/com/monkey/Q2_AddTwoNumber_v2.scala
+++ src/main/scala/com/monkey/Q2_AddTwoNumber_v2.scala
@@ -15,17 +15,22 @@ object Q2_AddTwoNumber_v2 extends App {
   printList(addTwoNumbers(l1, l2))
 
 
+
   def addTwoNumbers(l1: ListNode, l2: ListNode): ListNode = {
 
     var ll1 = l1
     var ll2 = l2
 
+    //定义指向头结点的变量
     var result: ListNode = new ListNode()
+    //定义一个dummy指针来指向头节点，这样可以任意的移动刚开始指向头节点的变量而不用担心头结点的丢失
     var dummy: ListNode = result
 
+    //保存两个一位数相加后结果的十位数上的值：结果大于等于10则为1，否则为0
     var carry: Int = 0
     var sum, x, y: Int = 0
     while (ll1 != null || ll2 != null) {
+      //如果当前链表的当前节点为空，则值为null
       if (ll1 != null) x = ll1.x else x = 0
       if (ll2 != null) y = ll2.x else y = 0
       sum = x + y + carry
@@ -33,12 +38,13 @@ object Q2_AddTwoNumber_v2 extends App {
       carry = if (sum > 9) 1 else 0
       result = result.next
 
+      //链表当前节点不为空，则向后推移一个节点，若为空，则不变
       if (ll1 != null) ll1 = ll1.next
       if (ll2 != null) ll2 = ll2.next
     }
-
+    //当两个链表中的所以节点都相加完之后，判断最后一个相加的结果是否超过10，超过10的时候需要额外增加一个节点来保存进位的结果
     if (carry == 1) result.next = new ListNode(1)
-
+    //返回dummy指针的next，即结果的头指针
     dummy.next
   }
 
--- src/main/scala/com/monkey/Q3_LongestSubstringWithoutRepeatingCharacters_v2.scala
+++ src/main/scala/com/monkey/Q3_LongestSubstringWithoutRepeatingCharacters_v2.scala
@@ -7,20 +7,26 @@ package com.monkey
   */
 object Q3_LongestSubstringWithoutRepeatingCharacters_v2 extends App {
 
-  println(lengthOfLongestSubstring2("dvdf"))
+  println(lengthOfLongestSubstringWithSet("dvdf"))
 
-  def lengthOfLongestSubstring(s: String): Int ={
+  def lengthOfLongestSubstringWithSet(s: String): Int ={
+    //传入的string的长度
     var length = s.length
+    //用set保存出现过的字符
     var elemSet: Set[Char] = Set()
 
     var begin, end = 0
+    //用来保存当前所计算的最长长度
     var maxLength = 0
     while(begin < length && end < length){
+      //如果set中不包含当前字符，则将当前字符添加入set，并向后移动给一个字符
       if(!elemSet.contains(s.charAt(end))){
         elemSet += s.charAt(end)
         end = end +1
+        //更新maxLength，此处是重点：注意一定要用max函数比较 当前的不重复字符串长度 与 上一次出现重复字符时记录的最长不重复字符串长度
         maxLength = Math.max(maxLength , end - begin)
       } else{
+        //如果set中包含当前字符，则利用循环将出现重复字符之前的字符全部从set中删除，保证set所留的都是需要重新计算长度的字符
         do {
           elemSet -= s.charAt(begin)
           begin = begin + 1
@@ -31,7 +37,8 @@ object Q3_LongestSubstringWithoutRepeatingCharacters_v2 extends App {
     maxLength
   }
 
-  def lengthOfLongestSubstring2(s: String): Int ={
+  //use map to save char and location
+  def lengthOfLongestSubstringWithMap(s: String): Int ={
     var length = s.length
 
     var map: Map[Char, Int] =Map()
@@ -49,7 +56,8 @@ object Q3_LongestSubstringWithoutRepeatingCharacters_v2 extends App {
     maxLength
   }
 
-  def lengthOfLongestSubstring3(s: String): Int ={
+  //use array index to indicate character
+  def lengthOfLongestSubstringWithArray(s: String): Int ={
     var length = s.length
 
     var index: Array[Int] = new Array[Int](128)
